384021f4ae973efe49a5a67a8a4318c6a503c082
sandeep <sandeepkumaraadha@gmail.com>
2020-06-19 15:51:50 +0530

    removed log statements

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Recipe = (props) => {
-  console.log(props);
+  //console.log(props);
   const label = props.recipe.label;
   const image = props.recipe.image;
   return (
