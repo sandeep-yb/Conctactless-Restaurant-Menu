602204b0ac3c141bf7526c0a6535804d7829b7fa
sandeep <sandeepkumaraadha@gmail.com>
2020-06-19 14:57:03 +0530

    removed build folder from gitignore

---
+++ build/precache-manifest.32941e82983c902480386288cc6f1434.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c177b04be88f43b75593380de770303",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5f1c0cc0f33068fa3fe",
+    "url": "/static/css/2.53823a3d.chunk.css"
+  },
+  {
+    "revision": "9770837fd71e26962548",
+    "url": "/static/css/main.ad563ea9.chunk.css"
+  },
+  {
+    "revision": "e5f1c0cc0f33068fa3fe",
+    "url": "/static/js/2.6101507d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6101507d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9770837fd71e26962548",
+    "url": "/static/js/main.ba618818.chunk.js"
+  },
+  {
+    "revision": "b3340baf3dd1cc0642b6",
+    "url": "/static/js/runtime-main.8ec033af.js"
+  }
+]);
